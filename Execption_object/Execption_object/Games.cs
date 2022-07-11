e7399000609dcf9f241ddfb5cbcb2298deb33628
Igorus666 <igor-sn@hotmail.com>
2022-07-11 19:51:20 -0300

    Add files via upload

---
+++ Execption_object/Execption_object/Games.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execption_object
+{
+    internal class Games
+    {
+        public string tittle;
+        public bool mult;
+        public int hours;
+        public string plat;
+
+        public void Name()
+        {
+            string resp;
+            Console.Write("\n\n\n   Enter the name of the game: ");
+            resp = Console.ReadLine();
+            tittle = resp;
+        }
+        public bool Multiplayer(bool trigger)
+        {
+            string resp;
+
+            Console.Write("   Is the game multiplayer [Y/N]: ");
+            resp = Console.ReadLine();
+
+            while (resp != "y" && resp != "Y" && resp != "n" && resp != "N")
+            {
+                Console.Clear();
+                trigger = true;
+                Console.Write("\n INVALID RESPONSE");
+                Thread.Sleep(1000);
+                Console.Write("\n please enter [Y/N]: ");
+                resp = Console.ReadLine();
+                Console.Clear();
+            }
+
+            if (resp == "n" || resp == "N")
+                mult = false;
+
+            else mult = true;
+
+            return trigger;
+        }
+
+        public void Time()
+        {
+            int resp;
+            Console.Write("   How many hours is the game: ");
+            while (!int.TryParse(Console.ReadLine(), out resp))
+            {
+                Console.Clear();
+                Console.Write("\n INVALID RESPONSE");
+                Thread.Sleep(1000);
+                Console.Write("\n please enter the number of hours of the game: ");
+            }
+
+            hours = resp;
+        }
+
+        public void Plataform()
+        {
+            string resp;
+            Console.Write("   For witch plataform is the game?\n\n      1-Playstation\n      2-Xbox\n      3-Nintendo\n");
+            resp = Console.ReadLine();
+
+            while (resp != "1" && resp != "2" && resp != "3")
+            {
+                Console.Clear();
+                Console.Write("\n INVALID RESPONSE");
+                Thread.Sleep(1000);
+                Console.Write("\n please enter: ");
+                Console.Write("\n      1-Playstation\n      2-Xbox\n      3-Nintendo\n");
+                resp = Console.ReadLine();
+            }
+
+
+            switch (resp)
+            {
+                case "1":
+                    plat = "Playstation";
+                    break;
+
+                case "2":
+                    plat = "Xbox";
+                    break;
+
+                case "3":
+                    plat = "Nintendo";
+                    break;
+            }
+
+        }
+
+    }
+}
---
+++ Execption_object/Execption_object/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execption_object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resp, plural = "";
+            bool trigger = false;
+            int libnum = 0, sequence = 0;
+            Games[] lib = new Games[100];
+
+
+            var Green = ConsoleColor.Green;
+            Console.ForegroundColor = Green;
+
+            Console.Write("\n\n\n       Well cum to to gamestop, would you");
+            Console.Write("\n    like to enter a game in the stock? [Y/N]: ");
+            resp = Console.ReadLine();
+
+            while (resp != "y" && resp != "Y" && resp != "n" && resp != "N")
+            {
+                Console.Clear();
+                Console.Write("\n INVALID RESPONSE");
+                Thread.Sleep(1000);
+                Console.Write("\n please enter [Y/N]: ");
+                resp = Console.ReadLine();
+                Console.Clear();
+            }
+
+            if(resp == "n" || resp == "N")
+            {
+                Console.Write("\n the FUCK you doing here them?");
+                Console.ReadKey();
+                return;
+            }          
+
+            for (int count=0; resp != "n" && resp != "N"; count++)
+            {
+                lib[count] = new Games();
+
+                lib[count].Name();
+                trigger = lib[count].Multiplayer(trigger);
+
+                if (trigger == true)
+                {
+                    trigger = false;
+                    Console.WriteLine("\n\n\n   Enter the name of the game: {0}", lib[count].tittle);
+                    if (lib[count].mult == true)
+                        Console.WriteLine("   The game is 1-4 multiplayer");
+                    else
+                        Console.WriteLine("   The game is singleplayer");
+                }
+
+                lib[count].Time();
+                lib[count].Plataform();
+
+                Console.Clear();
+                Console.Write("\n\n\n     Would you like to add another game? [Y/N]: ");
+                resp = Console.ReadLine();
+
+                libnum++;
+            }
+            if (libnum > 1)
+                plural = "s";
+
+            Console.Clear();
+            Console.WriteLine("\n\n\n     your library contains {0} game{1}", libnum, plural);
+
+            while (libnum > 0)
+            {
+                Console.WriteLine("\n     {0}º game\n", sequence+1);
+                Console.WriteLine("  Name: {0}", lib[sequence].tittle);
+
+                if (lib[sequence].mult == true)
+                   Console.WriteLine("  The game is 1-4 multiplayer");
+                else
+                Console.WriteLine("  The game is singleplayer");
+                Console.WriteLine("  Play time: {0} hours", lib[sequence].hours);
+                Console.WriteLine("  Plataform: {0}\n\n", lib[sequence].plat);
+
+                sequence++;
+                libnum--;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
